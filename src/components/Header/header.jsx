33e11d911c98fc95807417d6ad973b8aa3f33ea5
Kaleb Armbrust <kalebarmbrust@gmail.com>
2021-12-31 00:06:16 -0800

    imgs

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -10,7 +10,7 @@ export default class home extends Component {
                 <header class="p-3 bg-dark text-white">
                     <div class="container">
                     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                        {/* <img src={wflogo} alt="logo" class="nav-link px-2 text-white nav col-12 col-lg-auto logo"/> */}
+                        <img src={wflogo} alt="logo" class="nav-link px-2 text-white nav col-12 col-lg-auto logo"/>
                         <ul class="header-links nav col-12 col-lg-auto mb-2 justify-content-center mb-md-0">
                             <Link class="link" to="/"><li><a href="#" class="nav-link px-2 text-secondary">Home</a></li></Link>
                             <Link class="link" to="/about"><li><a href="#" class="nav-link px-2 text-white">About</a></li></Link>
