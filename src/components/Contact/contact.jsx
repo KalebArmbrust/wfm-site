1751cce9970d0220fc83c19f47874e89e895a9e3
Kaleb Armbrust <kalebarmbrust@gmail.com>
2021-12-31 15:43:53 -0800

    copy changes

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -13,11 +13,11 @@ export default class home extends Component {
                     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
                     <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
                         <h1 class="display-4 fw-bold lh-1">Need to get in touch?</h1>
-                        <p class="lead fw-bold">
+                        <p class="lead">
                             <br/>
                             You can contact our team by email for all of your inquiries.
                             <br/><br/>
-                            Email: worlfuelmgmt@gmail.com
+                            Email: worldfuelmgmt@gmail.com
                         </p>
                     </div>
                     <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
